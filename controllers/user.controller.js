715a536a6a6d4e9d565421a176679716b8bb3a6a
habibmufti <19102170@ittelkom-pwt.ac.id>
2024-05-15 21:06:34 +0700

    refactor: refactor input

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,26 @@
 const User = require("../models/user.model");
+/** user input
+ * {
+    "name": "John Doe",
+    "age": 25,
+    "gender": "Male",
+    "status": "Single",
+    "imgUrl": "https://example.com/images/john.jpg",
+    "username": "john_doe25",
+    "email": "john.doe@example.com",
+    "password": "password123",
+    "location": "New York, USA",
+    "bio": "Software developer with a passion for coding and technology.",
+    "preference": ["Hiking", "Reading", "Gaming"]
+  }
+ */
 
 class UserController {
   static async createUser(req, res, next) {
     try {
-      const { email, password } = req.body;
-      const user = await User.createUser({ email, password });
+      const input = req.body;
+      
+      const user = await User.createUser(input);
       res.status(201).json({message: "User created", user});
     } catch (err) {
       next(err);
