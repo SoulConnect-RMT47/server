d755cfffe851434fc0c11a10017bc883c7df1873
habib mufti mustofa <19102170@ittelkom-pwt.ac.id>
2024-05-17 17:37:46 +0700

    feat: Add updateUser endpoint
    The code changes add a new `updateUser` endpoint to the `UserController` class. This endpoint allows users to update their user information. The endpoint receives the updated user data in the request body, validates the input, and updates the user in the database. The response includes a success message and the updated user object.

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,6 @@ users.post("/login", UserController.login); // login
 users.use(authentication); // middleware
 
 users.get("/", UserController.getAllUsers); // get all users
+users.put("/", authentication, UserController.updateUser); // update user
 
 module.exports = users;
