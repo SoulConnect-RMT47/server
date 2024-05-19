b696af987ac1782898d3a9b6799c3f356aedbd40
habib mufti mustofa <19102170@ittelkom-pwt.ac.id>
2024-05-19 13:36:56 +0700

    refactor: Update MongoDB connection to use environment variables

--- connections/mongodb.js
+++ connections/mongodb.js
@@ -2,6 +2,6 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
 const client = new MongoClient(uri);
-const database = client.db(process.env.MONGODB_DBNAME_TEST || process.env.MONGODB_DBNAME || "gp-development");
+const database = client.db(process.env.MONGODB_DBNAME || process.env.MONGODB_DBNAME_TEST || "gp-development");
 
 module.exports = { client, database };
