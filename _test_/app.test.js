946547e254e85458fb3f40df0d649d4c2a040a64
habib mufti mustofa <105559875+habibmufti@users.noreply.github.com>
2024-05-19 15:14:42 +0700

    feat: add validation for preference and add testing for regsiter and login (#23)
    * refactor: Update user schema preference validation
    
    The code changes in the `user.schema.js` file update the `preference` field validation in the user schema. The previous implementation used `z.array(z.string())`, but it has been changed to `z.array({ message: "Preference is required" })` to provide a more descriptive error message. This change improves the clarity and usability of the validation for the `preference` field.
    
    * refactor: refactor jhon doe preference
    
    * feat: add testing for POST /user/register and POST /user/login

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -5,7 +5,8 @@ const { client, database } = require("../connections/mongodb");
 
 describe("App", () => {
   let server;
-  let token = "";
+  let tokenJohnDoe = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjQ5YjJmNWI0ZTY4ZGE4ZTJhYTFkMmUiLCJnZW5kZXIiOiJNYWxlIiwiaWF0IjoxNzE2MTA1OTczfQ.Ie-_q5G9YcM06lv4ia2JXJuh0WxoZT84TGIjUAjbdsE";
+  let tokenJaneSmith ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjQ5YjJmNWI0ZTY4ZGE4ZTJhYTFkMmUiLCJnZW5kZXIiOiJNYWxlIiwiaWF0IjoxNzE2MTA1OTczfQ.Ie-_q5G9YcM06lv4ia2JXJuh0WxoZT84TGIjUAjbdsE"
 
   const seedData = async () => {
     await UserCollection.insertMany(require("../data/testing/test.users.json"));
@@ -101,6 +102,24 @@ describe("App", () => {
       expect(response.statusCode).toBe(400);
       expect(response.body).toHaveProperty("message", "Email already exists");
     });
+    it("age is required should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: null,
+          gender: "Male",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe256",
+          email: "john.doe@example.com",
+          password: "password123",
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: ["Hiking", "Reading", "Gaming"],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Age is required");
+    });
 
     it("minimum age is 18 should respond with 400 status code for POST /users/register", async () => {
       const response = await request(app)
@@ -120,7 +139,166 @@ describe("App", () => {
       expect(response.statusCode).toBe(400);
       expect(response.body).toHaveProperty("message", "You must be at least 18 years old");
     });
+    it("user not select the gender should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: 18,
+          gender: "",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe256",
+          email: "john.doe1@example.com",
+          password: "password123",
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: ["Hiking", "Reading", "Gaming"],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Please select the gender");
+    });
+    it("password length too short should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: 18,
+          gender: "Male",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe25",
+          email: "john.doe1@example.com",
+          password: "pass",
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: ["Hiking", "Reading", "Gaming"],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Password must be at least 5 characters long");
+    });
+    it("password is null should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: 18,
+          gender: "Male",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe25",
+          email: "john.doe1@example.com",
+          password: null,
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: ["Hiking", "Reading", "Gaming"],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Password is required");
+    });
+    it("preference is not selected should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: 18,
+          gender: "Male",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe25",
+          email: "john.doe1@example.com",
+          password: "password123",
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: [],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Please select at least one preference");
+    });
+    it("selected preference is greater than 5 selected should respond with 400 status code for POST /users/register", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send({
+          name: "John Doe",
+          age: 18,
+          gender: "Male",
+          imgUrl: "https://example.com/images/john.jpg",
+          username: "john_doe25",
+          email: "john.doe1@example.com",
+          password: "password123",
+          location: "New York, USA",
+          bio: "Software developer with a passion for coding and technology.",
+          preference: ["Hiking", "Reading", "Gaming", "Traveling", "Photography", "Cooking", "Swimming"],
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "You can select up to 5 preferences");
+    });
   });
+
+  describe("POST /users/login", () => {
+    it("should respond with 200 status code for POST /users/login", async () => {
+      const response = await request(app).post("/users/login").send({
+        email: "jane.smith@example.com",
+        password: "password123",
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveProperty("message", "Login successful");
+      expect(response.body).toHaveProperty("token");
+      expect(response.body).toHaveProperty("user");
+    });
+    it("email is empty should respond with 400 status code for POST /users/login", async () => {
+      const response = await request(app)
+        .post("/users/login")
+        .send({
+          email: "",
+          password: "password123",
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Please enter a valid email");
+    });
+    it("email is null should respond with 400 status code for POST /users/login", async () => {
+      const response = await request(app)
+        .post("/users/login")
+        .send({
+          email: null,
+          password: "password123",
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Email is required");
+    });
+    it("invalid email should respond with 200 status code for POST /users/login", async () => {
+      const response = await request(app).post("/users/login").send({
+        email: "john.doe100@example.com",
+        password: "password123",
+      });
+      expect(response.statusCode).toBe(401);
+      expect(response.body).toHaveProperty("message", "Invalid email or password");
+    });
+    it("password is null should respond with 200 status code for POST /users/login", async () => {
+      const response = await request(app).post("/users/login").send({
+        email: "john.doe100@example.com",
+        password: null,
+      });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Password is required");
+    });
+    it("password is too short should respond with 200 status code for POST /users/login", async () => {
+      const response = await request(app).post("/users/login").send({
+        email: "john.doe100@example.com",
+        password: "pass",
+      });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("message", "Password must be at least 5 characters long");
+    });
+    it("password is invalid short should respond with 200 status code for POST /users/login", async () => {
+      const response = await request(app).post("/users/login").send({
+        email: "john.doe@example.com",
+        password: "password1234",
+      });
+      expect(response.statusCode).toBe(401);
+      expect(response.body).toHaveProperty("message", "Invalid email or password");
+    });
+
+
+  });
+
+  
 
   // Add more test cases as needed
 });
--- schema/user.schema.js
+++ schema/user.schema.js
@@ -5,14 +5,14 @@ const { z } = require("zod");
 const userSchema = z.object({
   name: z.string({ message: "Please enter a valid name" }),
   age: z.number({ message: "Age is required" }).min(18, { message: "You must be at least 18 years old" }),
-  gender: z.string({ message: "Gender is required" }),
+  gender: z.string({ message: "Gender is required" }).min(4, { message: "Please select the gender" }),
   imgUrl: z.string().optional(),
   username: z.string({ message: "Please input username" }).min(3),
   email: z.string({ message: "Email is required" }).email({ message: "Please enter a valid email" }),
   password: z.string({ message: "Password is required" }).min(5, { message: "Password must be at least 5 characters long" }),
   location: z.string().optional(),
   bio: z.string().optional(),
-  preference: z.array(z.string(), { message: "Please select at least one preference" })
+  preference: z.array(z.string()).min(1, { message: "Please select at least one preference" }).max(5, { message: "You can select up to 5 preferences" }),
 });
 
 const loginSchema = z.object({
