cc29b761f496833916a0afa4e5818d78378be31f
habib mufti mustofa <19102170@ittelkom-pwt.ac.id>
2024-05-20 17:07:40 +0700

    refactor: Remove console.log statements in user and swipe controllers

--- controllers/swipe.controller.js
+++ controllers/swipe.controller.js
@@ -12,11 +12,8 @@ class SwipeController {
     static async swipeUser(req, res, next) {
         try {
             const { swipeStatus } = req.body;
-            // console.log("🚀 ~ SwipeController ~ swipeUser ~ swipeStatus:", swipeStatus)
             const { id } = req.params;
-            // console.log("🚀 ~ SwipeController ~ swipeUser ~ id:", id)
             const loggedInUser = req.user;
-            // console.log("🚀 ~ SwipeController ~ swipeUser ~ loggedInUser:", loggedInUser)
 
             const swipe = await Swipe.addSwipe(id, loggedInUser, swipeStatus);
             res.status(200).json(swipe);
--- models/connection.model.js
+++ models/connection.model.js
@@ -5,50 +5,48 @@ class Connection {
   static async findAllConnecntion(loggedInUser) {
     try {
       const type = loggedInUser.gender === "Male" ? "user1" : "user2";
-    //   console.log("🚀 ~ Connection ~ findAllConnecntion ~ gender:", loggedInUser.gender);
-    //   console.log("🚀 ~ Connection ~ findAllConnecntion ~ type:", type);
-    const connections = await ConnectionCollection.aggregate([
+      const connections = await ConnectionCollection.aggregate([
         {
-            $match: {
-                [type]: new ObjectId(loggedInUser._id),
-            },
+          $match: {
+            [type]: new ObjectId(loggedInUser._id),
+          },
         },
         {
-            $lookup: {
-                from: "users",
-                let: { userId: type === "user1" ? "$user2" : "$user1" },
-                pipeline: [
-                    {
-                        $match: {
-                            $expr: {
-                                $eq: ["$_id", "$$userId"],
-                            },
-                        },
-                    },
-                    {
-                        $project: {
-                            password: 0,
-                        },
-                    },
-                ],
-                as: type === "user1" ? "user2" : "user1",
-            },
+          $lookup: {
+            from: "users",
+            let: { userId: type === "user1" ? "$user2" : "$user1" },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ["$_id", "$$userId"],
+                  },
+                },
+              },
+              {
+                $project: {
+                  password: 0,
+                },
+              },
+            ],
+            as: type === "user1" ? "user2" : "user1",
+          },
         },
         {
-            $unwind: "$user1",
+          $unwind: "$user1",
         },
         {
-            $unwind: "$user2",
+          $unwind: "$user2",
         },
-    ]).toArray();
+      ]).toArray();
 
-    const result = connections.map((connection) => {
+      const result = connections.map((connection) => {
         const user = type === "user1" ? connection.user2 : connection.user1;
         user.messageID = connection.messageID;
         return user;
-    });
+      });
 
-    return result;
+      return result;
     } catch (err) {
       throw err;
     }
